3043992c86a293bb22b0a4b1c1f456b6c89d5a0a
philhannant <philip.a.hannant@gmail.com>
2018-02-18 14:31:23 +0000

    changed tree to int

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,13 +1,36 @@
 package tree;
 
-public class Tree<T> {
+public class Tree {
+
+    private Node root;
+
+    public Tree(int rootData){
+        if (root == null) {
+            this.root = new Node(null, null, rootData);
+        }
+    }
+
+    public void addNode(int data){
+        addNodeHelper(data, root);
+    }
+
+    public void addNodeHelper(int data, Node currentNode){
+        if(data < currentNode.data){
+
+        }
+    }
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+        private int data;
 
 
-
-
-    private class Node<T>{
-
-        private Node<T> left;
-        private Node<T> right;
+        public Node(Node left, Node right, int data){
+            this.left = left;
+            this.right = right;
+            this.data = data;
+        }
     }
 }
