e306e60c3c7c968ed7c817c7d09637d843788fdd
Phil Hannant <philip.a.hannant@gmail.com>
2018-02-19 12:51:00 +0000

    added leftChild, rightChild and parent

--- src/tree/Heap.java
+++ src/tree/Heap.java
@@ -4,10 +4,32 @@ public class Heap {
 
     private int size;
     private int capacity;
-    private Object[] harr;
+    private int[] harr;
 
     public Heap(int capacity){
         this.capacity = capacity;
-        harr = new Object[capacity];
+        harr = new int[capacity];
     }
+
+
+    public int getMin(){
+        return harr[0];
+    }
+
+    public int parent(int n){
+        return (n-1)/2;
+    }
+
+    public int leftChild(int n){
+        return (2*n+1);
+    }
+
+    public int rightChild(int n){
+        return (2*n+2);
+    }
+
+    public void insertKey(){
+
+    }
+
 }
