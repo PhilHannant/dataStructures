3e4c036bdf6829f451f403c69fdcf58a3a2bc79f
Phil Hannant <philip.a.hannant@gmail.com>
2018-02-19 13:15:11 +0000

    working on insert

--- src/tree/Heap.java
+++ src/tree/Heap.java
@@ -4,11 +4,12 @@ public class Heap {
 
     private int size;
     private int capacity;
-    private int[] harr;
+    private Integer[] harr;
 
     public Heap(int capacity){
         this.capacity = capacity;
-        harr = new int[capacity];
+        harr = new Integer[capacity];
+        size = 0;
     }
 
 
@@ -16,20 +17,33 @@ public class Heap {
         return harr[0];
     }
 
-    public int parent(int n){
-        return (n-1)/2;
+    public int parent(int index){
+        return (index-1)/2;
     }
 
-    public int leftChild(int n){
-        return (2*n+1);
+    public int leftChild(int index){
+        return (2*index+1);
     }
 
-    public int rightChild(int n){
-        return (2*n+2);
+    public int rightChild(int index){
+        return (2*index+2);
     }
 
-    public void insertKey(){
+    public void insertKey(Integer key){
+        if(size == capacity){
+            return;
+        }
+        harr[size] = key;
+        size++;
+        if(harr[parent(size-1)] > key){
+            swap(size-1);
+        }
+    }
 
+    public void swap(int index){
+        int parent = harr[parent(index)];
+        int child = harr[index];
+        while
     }
 
 }
