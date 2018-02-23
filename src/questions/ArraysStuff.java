bc59c4510d5685a4b94bdf37a2feca6033c58244
Phil Hannant <philip.a.hannant@gmail.com>
2018-02-23 14:47:57 +0000

    merge works, used sort

---
+++ src/questions/ArraysStuff.java
@@ -0,0 +1,42 @@
+package questions;
+
+import java.util.Arrays;
+
+public class ArraysStuff {
+
+    public void missingNumbers(int[] arr){
+        int sumMissing = 0;
+        int sumTotal = 0;
+        int max = 0;
+        for(int i = 0; i < arr.length; i++){
+            sumMissing += arr[i];
+            max = arr[i];
+        }
+        for(int j = 0; j < max; j++){
+            sumTotal += j+1;
+        }
+
+
+        int difference = sumTotal - sumMissing;
+
+        System.out.println(sumMissing);
+        System.out.println(sumTotal);
+        System.out.println(difference);
+    }
+
+    public int[] mergeArrays(int[] arr1, int[] arr2){
+        int start = 0;
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] == 0){
+                start = i;
+                break;
+            }
+        }
+        for(int j = start; j< arr1.length; j++){
+            arr1[j] = arr2[(j-arr2.length)];
+        }
+        Arrays.sort(arr1);
+        return arr1;
+    }
+
+}
