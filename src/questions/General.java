175aa14a9c5fe7b9ccc0299467d7419a57047c7a
Phil Hannant <PhilHannant@users.noreply.github.com>
2018-02-23 15:58:50 +0000

    added sums

--- src/questions/General.java
+++ src/questions/General.java
@@ -4,6 +4,10 @@ public class General {
 
     public int clockAngle(int hour, int minute){
         int angle = 0;
+        int hourAngle = hour * 30;
+        int minuteAngle = minute * 6;
+        int extraAngle = (minute/60)*30;
+        hourAngle+= extraAngle;
         return angle;
     }
 }
