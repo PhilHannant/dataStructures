f458a7027ea3f620f852e807c25b6f06eb2a9b84
philhannant <philip.a.hannant@gmail.com>
2018-02-26 21:12:39 +0000

    matric printer added

--- src/questions/QuestionsMain.java
+++ src/questions/QuestionsMain.java
@@ -21,6 +21,9 @@ public class QuestionsMain {
 
         int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
         a.printSpiral(matrix);
+        System.out.println("next");
+        int[][] matrix2 = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
+        a.printSpiral(matrix2);
     }
 
 }
